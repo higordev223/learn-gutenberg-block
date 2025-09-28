d21f6cb58008a50b85e79f54c7c26bba213fb060
higordev223 <ruslanus866@outlook.com>
2025-09-27 21:21:24 -0400

    create curvy block and change plugin name and add curvy on side panel

---
+++ build/blocks-manifest.php
@@ -0,0 +1,25 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'curvy' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'blockylicious/curvy',
+		'version' => '0.1.0',
+		'title' => 'Curvy',
+		'category' => 'widgets',
+		'icon' => 'smiley',
+		'description' => 'Curvy shape dividers to make dividing page content more interesting',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'blockylicious',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ build/curvy/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c661cd6c17ae9f96f94f');
